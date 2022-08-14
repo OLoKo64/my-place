502f55a43dc5a5f1cd5aea123a217359bee29738
OLoKo64 <naldoepingo1@gmail.com>
2022-08-14 17:23:45 -0300

    feat: fix typos and add more information

--- src/pages/Timeline.vue
+++ src/pages/Timeline.vue
@@ -27,8 +27,10 @@ export default {
           content:
             "Here I tell why I created this website, the technologies I used and the things I want to improve.",
           fullContent: [
-            "This is my second website, which I made using the tools I know about. The first one was made using React and was way more complex than it should be.",
-            "This one focus on the basic features first, using tools that are made for this type of content. Then I will add more features and improve them.",
+            "This is my second website, which I made using the tools I know at the moment. The first one was made using React and was way more complex than it should had been.",
+            "This one focus on the basic features first, using tools that are made for this type of content. Then later on I will add more features as I need them.",
+            "It was created using Gridsome, a really cool Jamstack framework for building static generated websites using Vue under the hud.",
+            "I was not happy with my old website and it was very hard to update it, so I decided to create a new one. I used Vue and Gridsome for the frontend and a simple file server to provide the data for this timeline.",
           ],
           readTime: "1 min read",
           readText: "Read More...",
