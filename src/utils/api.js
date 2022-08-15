33b5eedd36ec7a4c2e9598e90012ed810a39e490
OLoKo64 <naldoepingo1@gmail.com>
2022-08-15 10:39:51 -0300

    feat: change server logic to github

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,40 +1,25 @@
 import axios from "axios";
 
-async function fetchIndexFilesData() {
+async function fetchTimelineData() {
   try {
     const { data } = await axios.get(
-      `${process.env.GRIDSOME_BASE_HOST}/${process.env.GRIDSOME_FILE_SERVER_INDEX}`
+      `${process.env.GRIDSOME_TIMELINE_DATA_URL}`
     );
+    console.log(data);
     if (!data) {
       return [];
     }
-    return data.contents
-  } catch (_) {
-    return [];
-  }
-}
-
-async function fetchTimelineData() {
-  try {
-    const files = await fetchIndexFilesData();
-    const fileData = await Promise.all(
-      files["timeline"].map((file) =>
-        axios.get(`${process.env.GRIDSOME_BASE_HOST}/${process.env.GRIDSOME_FILE_SERVER_TIMELINE}${file}`)
-      )
-    );
-    return fileData
-      .map((file) => file.data)
-      .map((fileData) => {
-        return {
-          title: fileData.title,
-          datePublished: fileData["date-published"],
-          content: fileData.content,
-          fullContent: fileData["full-content"],
-          readTime: fileData["read-time"],
-          readText: fileData["read-text"],
-          readLink: fileData["read-link"],
-        };
-      });
+    return data.map((fileData) => {
+      return {
+        title: fileData.title,
+        datePublished: fileData["date-published"],
+        content: fileData.content,
+        fullContent: fileData["full-content"],
+        readTime: fileData["read-time"],
+        readText: fileData["read-text"],
+        readLink: fileData["read-link"],
+      };
+    });
   } catch (_) {
     return [];
   }
